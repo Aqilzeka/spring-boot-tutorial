563027784b9665478f239fb31ecbe3a243c24298
Aqil <aqilaghamirzayev@gmail.com>
2022-06-08 16:29:58 +0400

    Add asynchronous-threading module

---
+++ asynchronous-threading/src/main/java/com/learn/asynchronous/AsynchronousThreadingApplication.java
@@ -0,0 +1,13 @@
+package com.learn.asynchronous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousThreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchronousThreadingApplication.class, args);
+	}
+
+}
